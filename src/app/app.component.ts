7b52cfa9f1b1612bb1d8250ce39b8ee7f7324162
Colin <colin.marti@students.bfh.ch>
2025-02-28 12:37:18 +0100

    Grundstruktur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MeComponent } from './me/me.component';
+import { CvComponent } from './cv/cv.component';
+import { InterestsComponent } from './interests/interests.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [MeComponent, CvComponent, InterestsComponent, HomepageComponent],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'MeinePortfolioSeite';
 }
