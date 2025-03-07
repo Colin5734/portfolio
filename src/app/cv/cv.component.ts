3713086196e1e13c197dc03772cd0fecc76a59a7
Colin <colin.marti@students.bfh.ch>
2025-03-07 10:55:08 +0100

    added cv

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface TimelineItem {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+  type: 'Berufserfahrung' | 'Ausbildung';
+}
 
 @Component({
   selector: 'app-cv',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cv.component.html',
-  styleUrl: './cv.component.css'
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent {
-
+  /**
+   * Die Reihenfolge in diesem Array legt fest,
+   * wer ganz links bzw. weiter rechts ist.
+   */
+  timelineItems: TimelineItem[] = [
+    // Links beginnend:
+    {
+      id: 1,
+      title: 'Kaufmann EFZ',
+      date: '2014 - 2017',
+      description: 'WKS Bern',
+      type: 'Ausbildung',
+    },
+    {
+      id: 2,
+      title: 'Berufsmatur Typ Wirtschaft',
+      date: '2018 - 2019',
+      description: 'WKS Bern',
+      type: 'Ausbildung',
+    },
+    {
+      id: 3,
+      title: 'Sachbearbeiter',
+      date: '04.2020 - 08.2020',
+      description: 'Eidg. Steuerverwaltung',
+      type: 'Berufserfahrung',
+    },
+    {
+      id: 4,
+      title: 'Administrativer Assistent',
+      date: '10.2020 - 07.2024',
+      description: 'Bundesamt für Gesundheit',
+      type: 'Berufserfahrung',
+    },
+    {
+      id: 5,
+      title: 'Praktikum IT Security',
+      date: '08.2024 - heute',
+      description: 'Steuerverwaltung Kt. Bern',
+      type: 'Berufserfahrung',
+    },
+    {
+      id: 6,
+      title: 'BSc Wirtschaftsinformatik',
+      date: '2022 - 2026',
+      description: 'Berner Fachhochschule',
+      type: 'Ausbildung',
+    },
+    // Rechts endend
+  ];
 }
