2066d7be7abd3b51a3366c42f4fd793ef73813c9
Colin <colin.marti@students.bfh.ch>
2025-03-28 09:33:43 +0100

    edit interests and added quiz comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { MeComponent } from './me/me.component';
 import { CvComponent } from './cv/cv.component';
 import { InterestsComponent } from './interests/interests.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [MeComponent, CvComponent, InterestsComponent, HomepageComponent],
+  imports: [
+    MeComponent,
+    CvComponent,
+    InterestsComponent,
+    HomepageComponent,
+    QuizComponent,
+  ],
 })
 export class AppComponent {
   title = 'MeinePortfolioSeite';
